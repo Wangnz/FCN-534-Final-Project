19222501c22d300878d28ccef47f4b4383dc80a1
yoonsang0910 <32704756+yoonsang0910@users.noreply.github.com>
2018-12-01 15:08:49 -0500

    trilateration update
    - added few more conditions for inner circle (one circle including the other)
    - getting the intersecting point based on the average coordinate of three points (center1, center2, intersecting line's centroid)

--- testing.py
+++ testing.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 import time
 import math
+from coordinate import Coordinate2D
 
 
 def line(p1, p2):
@@ -26,8 +27,15 @@ def intersection(L1, L2):
         return False
 
 
+# Euclidean distance
 def calc_dist(p1, p2):
     return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
+# Average
+def calc_avg_pos(center_point_1, center_point_2, intersection_point):
+    return ((center_point_1.x + center_point_2.x + intersection_point.x) / 3,
+            (center_point_1.y + center_point_2.y + intersection_point.y) / 3)
 
 
 # param : ap1, ar2 = data dictionary
@@ -37,8 +45,10 @@ def ap_circle_condition_check(ap1, ap2):
     radius_2 = ap2['dist']  # AP_2 's radius
 
     # inner circle check(one circle is inside the other)
-    if radius_1 > dist or radius_2 > dist:
+    if dist < abs(radius_1 - radius_2):
         return False
+    # if radius_1 > dist or radius_2 > dist: # partial inner
+    #     return False
 
     # separate circle check
     if dist > radius_1 + radius_2:
@@ -53,41 +63,49 @@ def ap_circle_condition_check(ap1, ap2):
 
 # param : ap1, ar2 = data dictionary
 def get_intersecting_points(ap1, ap2):
-    dist = calc_dist((ap1['x'], ap1['y']), (ap2['x'], ap2['y']))  # distance of the two circles
-    radius_1 = ap1['dist']  # AP_1's radius
-    radius_2 = ap2['dist']  # AP_2 's radius
+    if ap_circle_condition_check(ap1, ap2) == False:
+        print 'skip_checking since the circle does not qualify'
+        # skip the test
+    else:
+        dist = calc_dist((ap1['x'], ap1['y']), (ap2['x'], ap2['y']))  # distance of the two circles
+        radius_1 = ap1['dist']  # AP_1's radius
+        radius_2 = ap2['dist']  # AP_2 's radius
 
-    sub_x = ap2['x'] - ap1['x']
-    sub_y = ap2['y'] - ap1['y']
-    tmp = (radius_1 * radius_1 - radius_2 * radius_2 + dist * dist) / (2 * dist)
-    tmp_2 = math.sqrt(radius_1 * radius_1 - tmp * tmp)
-    xm = ap1['x'] + tmp * sub_x / dist
-    ym = ap1['y'] + tmp * sub_y / dist
-    pt_x1 = xm + tmp_2 * sub_y / dist
-    pt_x2 = xm - tmp_2 * sub_y / dist
-    pt_y1 = ym - tmp_2 * sub_x / dist
-    pt_y2 = ym + tmp_2 * sub_x / dist
+        sub_x = ap2['x'] - ap1['x']
+        sub_y = ap2['y'] - ap1['y']
+        tmp = (radius_1 * radius_1 - radius_2 * radius_2 + dist * dist) / (2 * dist)
+        tmp_2 = math.sqrt(radius_1 * radius_1 - tmp * tmp)
+        xm = ap1['x'] + tmp * sub_x / dist
+        ym = ap1['y'] + tmp * sub_y / dist
+        pt_x1 = xm + tmp_2 * sub_y / dist
+        pt_x2 = xm - tmp_2 * sub_y / dist
+        pt_y1 = ym - tmp_2 * sub_x / dist
+        pt_y2 = ym + tmp_2 * sub_x / dist
 
     return (pt_x1, pt_y1), (pt_x2, pt_y2)
 
 
 if __name__ == "__main__":
 
-    ap1 = {'dist': 1, 'x': 0, 'y': 0}
-    ap2 = {'dist': 2, 'x': 0, 'y': -2}
+    ap1 = {'dist': 4, 'x': 0, 'y': 0}
+    ap2 = {'dist': 2, 'x': 3, 'y': 0}
     L1 = line((ap1['x'], ap1['y']), (ap2['x'], ap2['y']))
     L2 = line([-5, 1], [5, 1])
 
-    if ap_circle_condition_check(ap1, ap2) == False:
-        print 'skip_checking since the circle does not qualify'
-        # skip the test
+    intersect_point_1, intersect_point_2 = get_intersecting_points(ap1, ap2)
+    ap_center_line = line((ap1['x'], ap1['y']), (ap2['x'], ap2['y']))
+    intersect_point_line = line(intersect_point_1, intersect_point_2)
+    intersection_centroid = (0.0, 0.0)
+    # intersecting in only one point
+    if intersect_point_1 == intersect_point_2:
+        intersection_centroid = intersect_point_1
     else:
-        intersect_point_1, intersect_point_2 = get_intersecting_points(ap1, ap2)
-        ap_center_line = line((ap1['x'], ap1['y']), (ap2['x'], ap2['y']))
-        intersect_point_line = line(intersect_point_1, intersect_point_2)
-        print intersect_point_1, intersect_point_2
-        print ap_center_line
-
         intersection_centroid = intersection(ap_center_line, intersect_point_line)
-        if intersection_centroid:
-            print "Intersection detected:", intersection_centroid
+    if intersection_centroid:
+        print "Intersection detected:", intersection_centroid
+    print (ap1['x'], ap1['y'])
+    print (ap2['x'], ap2['y'])
+
+    res = calc_avg_pos(Coordinate2D((ap1['x'], ap1['y'])), Coordinate2D((ap2['x'], ap2['y'])),
+                       Coordinate2D(intersection_centroid))
+    print("X: %.4f, Y:%.4f" % (res[0], res[1]))
