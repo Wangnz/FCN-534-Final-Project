e0ab97c97b3a61ef2cb8451288acc928a63e0c23
yoonsang0910 <32704756+yoonsang0910@users.noreply.github.com>
2018-12-02 16:49:47 -0500

    updated client-side
    - improved efficiency (less loop)
    - added recording function : creates a signal record file under 'Record' dir.
    - platform check (limited to macOS)
    - modified the code into more editable format

--- readData.py
+++ readData.py
@@ -1,10 +1,14 @@
 import json
 import numpy as np
 import subprocess
+import os
+import platform
+from datetime import datetime
 
 
 def get_access_points():
-    cmd = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -s"
+    # cmd = "netsh wlan show networks mode=bssid"  # Windows (TO USE, DIFF PARSING REQUIRED)
+    cmd = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -s"  # Mac
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     (out, _) = proc.communicate()
 
@@ -23,7 +27,33 @@ def get_ap_group(AP_list, target_bssid):
     return group
 
 
+def get_filename(timestamp):
+    cur_date = timestamp.date()
+    cur_time = timestamp.time()
+
+    # YYMMDD HHMMSS format
+    filename = "{0}{1:02d}{2:02d}_{3:02d}{4:02d}{5:02d}".format(str(cur_date.year)[-2:], cur_date.month,
+                                                                cur_date.day, cur_time.hour,
+                                                                cur_time.minute,
+                                                                cur_time.second)
+    return filename
+
+
+def as_record_format(ap_data):
+    return "{0:2s} | {1} | {2} | {3}\n".format(ap_data['RSSI'], ap_data['GROUP'], ap_data['BSSID'], ap_data['SSID'])
+
+
+def platform_check():
+    operating_system = platform.system().lower()
+    if operating_system != 'darwin':
+        print("Mac OS platform required")
+        exit(1)
+
+
 if __name__ == "__main__":
+    # only run it under mac OS
+    platform_check()
+
     AP_list = {
         'H1': ['7a:e0', '7a:e1', '7a:e3', '7a:f0', '7a:f1', '7a:f3'],
         'H2': ['77:00', '77:01', '77:03', '77:10', '77:11', '77:13'],
@@ -36,25 +66,50 @@ if __name__ == "__main__":
         'S1': ['85:a0', '85:a1', '85:a3', '85:b0', '85:b1', '85:b3'],
         'S2': ['e1:60', 'e1:62', 'e1:63', 'e1:70', 'e1:72', 'e1:73']
     }
-    while True:
-        raw_ap_list = get_access_points()
-        raw_ap_list_2 = raw_ap_list.split('\n')
-        filtered_ap_list = []
-        for index in xrange(1, len(raw_ap_list_2)):
-            signal_data = {}
-            raw_ap = raw_ap_list_2[index].strip().split(' ')
+    timestamp = datetime.now()
+    filename_record_time = get_filename(timestamp)
+    iter_cnt = 0
 
-            # excluding signals that are not useful (e.g. printers)
-            if raw_ap[0].startswith('cs_'):
-                signal_data['SSID'] = raw_ap[0]
-                signal_data['BSSID'] = raw_ap[1][-5:].upper()
-                signal_data['RSSI'] = raw_ap[2]
-                group_res = get_ap_group(AP_list, raw_ap[1][-5:])
-                if group_res == 'X':
-                    print("Such WiFi signal not found")
-                signal_data['GROUP'] = group_res
-                filtered_ap_list.append(signal_data)
-                # final_json = json.dumps(final)
+    if not os.path.exists('./Record'):
+        os.makedirs('./Record')
 
-        # sort by signal strength (RSSI)
-        sorted_ap_list = sorted(filtered_ap_list, key=lambda e: e['RSSI'], reverse=False)
+    with open('./Record/Record(' + filename_record_time + ').txt', 'w') as f:
+        f.write(str(timestamp) + '\n')
+        f.write("START\n")
+        while True:
+            iter_cnt += 1
+            f.write("-----------------------------------------------------------------Iter " + str(iter_cnt) + "\n")
+            try:
+                raw_ap_list = get_access_points()
+                raw_ap_list_2 = raw_ap_list.split('\n')
+                filtered_ap_list = []
+
+                for index in xrange(1, len(raw_ap_list_2)):
+                    signal_data = {}
+                    raw_ap = raw_ap_list_2[index].strip().split(' ')
+
+                    # excluding signals that are not useful (e.g. printers)
+                    if raw_ap[0].startswith('cs_'):
+                        signal_data['SSID'] = raw_ap[0]
+                        signal_data['BSSID'] = raw_ap[1][-5:].upper()
+                        signal_data['RSSI'] = raw_ap[2]
+                        group_res = get_ap_group(AP_list, raw_ap[1][-5:])
+                        if group_res == 'X':
+                            print("Such WiFi signal not found")
+                        signal_data['GROUP'] = group_res
+                        filtered_ap_list.append(signal_data)
+                        # final_json = json.dumps(final)
+
+                # sort by signal strength (RSSI)
+                sorted_ap_list = sorted(filtered_ap_list, key=lambda e: e['RSSI'], reverse=False)
+
+                # convert into recording format and record signals
+                for ap_data in sorted_ap_list:
+                    ap_data_record = as_record_format(ap_data)
+                    f.write(ap_data_record)
+            except Exception as e:
+                f.write("[ERROR] : " + str(e.message) + "\n")
+                print("[ERROR] :%s" % str(e.message))
+                break
+
+        f.write("FINISH")
